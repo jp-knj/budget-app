6fef62366294aec452dbd6d0807fbef507a3331c
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-03-06 23:32:37 +0900

    :art: Add styles to good design
    t

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import { GlobalProvider } from './context/GlobalState.jsx'
+
+// Pages
+import Dashboard from './pages/Dashboard'
+import Transactions from './pages/Transactions'
+import Statistics from './pages/Statistics'
+import Profile from './pages/Profile'
+import Login from './pages/Login'
+import Logout from './pages/Logout'
+
+// Components
+import Sidebar from './components/Sidebar'
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <GlobalProvider>
+      <Sidebar>
+        <Switch>
+          <Route path='/login' component={Login} />
+          <Route path='/logout' component={Logout} />
+          <Route path='/dashboard' component={Dashboard} />
+          <Route path='/transactions' component={Transactions} />
+          <Route path='/statistics' component={Statistics} />
+          <Route path='/profile' component={Profile} />
+        </Switch>
+      </Sidebar>
+    </GlobalProvider>
   )
 }
 export default App
