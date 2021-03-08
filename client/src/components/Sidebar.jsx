bd0bf2a361a3974656312af2289ba1850878a678
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-03-08 12:36:49 +0900

    :+1: Add btn and create styles
    t

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,10 +1,56 @@
 import React from 'react'
-import { withRouter } from 'react-router-dom'
+import { NavLink, withRouter } from 'react-router-dom'
 
-const Sidebar = ({children}) => {
+// Material-UI Core
+import { MenuItem, MenuList, ListItemIcon } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+// Material-UI Icon
+import { Add } from '@material-ui/icons'
+
+const Sidebar = ({ children }) => {
+  const useStyles = makeStyles(() => ({
+    addCircleIcon: {
+      color: 'white'
+    }
+  }));
+  const classes = useStyles();
   return (
-    <div>
-      {children}
+    <div className="wrapper">
+      <div className="sidebar">
+        <aside>
+          <div className="">
+            <h3>Friday, 5 March</h3>
+            <h4>Good Morning</h4>
+            <h2>Kenji Tomita</h2>
+          </div>
+          <nav className="navigation">
+            <MenuList>
+              <MenuItem to="/dashboard" component={NavLink}>
+                <ListItemIcon>DashBoard</ListItemIcon>
+              </MenuItem>
+              <MenuItem to="/transaction" component={NavLink}>
+                <ListItemIcon>Transactions</ListItemIcon>
+              </MenuItem>
+              <MenuItem to="/statistics" component={NavLink}>
+                <ListItemIcon>Statistics</ListItemIcon>
+              </MenuItem>
+              <MenuItem to="/profile" component={NavLink}>
+                <ListItemIcon>Profile</ListItemIcon>
+              </MenuItem>
+              <MenuItem to="/logout" component={NavLink}>
+                <ListItemIcon>Logout</ListItemIcon>
+              </MenuItem>
+            </MenuList>
+          </nav>
+        </aside>
+      </div>
+      <main className="container">
+        {children}
+      </main>
+      <div className="btn">
+        <Add className={`${classes.addCircleIcon}`} />
+      </div>
     </div>
   )
 }
