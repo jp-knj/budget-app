6afde4a86b90b934d5d2c41cbf8d3b423649829e
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-03-08 12:20:15 +0900

    :+1: Markup budget
    t

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, {Fragment} from 'react'
+
+import TransactionList from '../components/TransactionList'
 
 const Dashboard = () => {
   return (
-    <div>
-      Dashboard
-    </div>
+    <Fragment>
+      <header className='header'>
+        <section className="budget">
+          <div className="budget_inner">
+              <p className="budget_title">Total Balance</p>
+              <span className="budget_value">5000円</span>
+            <div className='budget_box'>
+              <div className='budget_items'>
+                <p className="budget_title">Income</p>
+                <span className="budget_value-sub">5000円</span>
+              </div>
+              <div className='budget_items'>
+                <p className="budget_title">Expense</p>
+                <span className="budget_value-sub">5000円</span>
+              </div>
+            </div>
+          </div>
+        </section>
+      </header>
+      <TransactionList/>
+    </Fragment>
   )
 }
 export default Dashboard
