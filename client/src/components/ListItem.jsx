e360b3f2a0898b137ed4bddcc22d62c3c5b2f081
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-03-09 23:34:27 +0900

    :art: Add animation to display edit btn
    t

--- client/src/components/ListItem.jsx
+++ client/src/components/ListItem.jsx
@@ -1,6 +1,5 @@
-import React, { useState, useContext, useCallback, Fragment } from 'react'
+import React, { useState, useCallback, Fragment } from 'react'
 import { useSpring, config, animated } from 'react-spring'
-import { GlobalContext } from '../context/GlobalState'
 
 // Material Ui
 import DeleteSharpIcon from '@material-ui/icons/DeleteSharp'
@@ -14,21 +13,61 @@ import TransactionForm from './TransactionForm'
 const ListItem = ({ data }) => {
   const [showMenu, setshowMenu] = useState(false),
         [open, setOpen] = useState(false);
+  const [deleted, setDeleted] = useState(false);
   const formatDate = (date) => moment(date).format('D MMM, YYYY')
   const sign = !data.amount ? null : data.amount > 0 ? '+' : '',
         color = !data.amount ? 'zero' : data.amount > 0 ? 'positive' : 'negative';
+
+  const buttonWidth = 70,
+        paddingLeft = 15,
+        totalWidth = buttonWidth * 2,
+        travelY = paddingLeft + totalWidth;
+
+  const menuBlock = {
+    position: 'absolute',
+    left: `-${travelY}px`,
+    width: totalWidth,
+    height: '100%',
+  };
+
+  const menuBtn = {
+    width: buttonWidth,
+    height: '100%',
+    display: 'inline-block',
+    position: 'relative',
+    cursor: 'pointer',
+  };
+
+  const props = useSpring({
+    opacity: deleted ? 0 : 1,
+    transform: showMenu
+      ? `translate3d(${travelY}px, 0, 0)`
+      : deleted
+      ? 'translate3d(-2000px,0,0)'
+      : 'translate3d(0px,0,0)',
+    config: config.stiff,
+    cursor: 'pointer',
+  });
+
+  const handleShowMenu = useCallback(() => {
+    setshowMenu(!showMenu);
+  }, [showMenu]);
 
   const handleShowForm = useCallback(() => {
     setOpen(true);
   }, [])
   return (
     <Fragment>
-      <animated.li className='transaction_list'>
-        <div>
-          <div>
+      <animated.li
+        className='transaction_list'
+        style={props}
+        onClick={handleShowMenu}
+      >
+        <div style={menuBlock} >
+          <div style={menuBtn} >
             <DeleteSharpIcon/>
           </div>
-          <div onClick={handleShowForm}>
+          <div style={menuBtn} onClick={handleShowForm}>
             <EditSharpIcon/>
           </div>
         </div>
