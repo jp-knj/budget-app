c55dc46b4aebeaf8cdfeb428346784d1e9b178cf
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-03-14 21:54:33 +0900

    :+1: Add format and calculation
    t

--- client/src/utils/calculation.js
+++ client/src/utils/calculation.js
@@ -1,8 +1,76 @@
-import moment from 'moment'
+import Moment from 'moment'
+import { extendMoment } from 'moment-range'
+
+const moment = extendMoment(Moment);
+
+export function getGreeting(d) {
+  const hour = moment(d).hour()
+  if (hour < 4) return 'night'
+  if (hour < 13) return 'morning'
+  if (hour < 18) return 'afternoon'
+  if (hour < 24) return 'evening'
+  return 'day'
+}
+
+export function checkDayTime(d) {
+  const hour = moment(d).hour();
+  if (hour > 3 && hour < 18) return true;
+  return false;
+}
+
+export function checkDay(d) {
+  const start = moment().startOf('day');
+  const end = moment().endOf('day');
+  const date = moment(d);
+  const range = moment().range(start, end);
+
+  if (date.within(range)) return true;
+  return false;
+}
 
 export function checkRecent(d) {
+  const start = moment().subtract(3, 'days');
+  const end = moment().add(3, 'days');
   const date = moment(d);
-  if (date) return true;
+  const range = moment().range(start, end);
+
+  if (date.within(range)) return true;
+  return false;
+}
+
+export function checkWeek(d) {
+  const sunday = moment().startOf('week');
+  const saturday = moment().endOf('week');
+  const date = moment(d);
+  const range = moment().range(sunday, saturday);
+
+  if (date.within(range)) return true;
+  return false;
+}
+
+export function getWeekDate(weekNum) {
+  const sunday = (weekNum) => moment().day(0).week(weekNum).format('D');
+  const saturday = (weekNum) => moment().day(6).week(weekNum).format('D MMM');
+  return `${sunday(weekNum)} - ${saturday(weekNum)}`;
+}
+
+export function checkMonth(d) {
+  const fistday = moment().startOf('month');
+  const lastday = moment().endOf('month');
+  const date = moment(d);
+  const range = moment().range(fistday, lastday);
+
+  if (date.within(range)) return true;
+  return false;
+}
+
+export function checkYear(d) {
+  const fistday = moment().startOf('year');
+  const lastday = moment().endOf('year');
+  const date = moment(d);
+  const range = moment().range(fistday, lastday);
+
+  if (date.within(range)) return true;
   return false;
 }
 
@@ -45,4 +113,14 @@ export function sortDateAmount(a, b, sortColumn, sortLatest, sortDsc) {
   return sortDsc
     ? sortAmountDsc(a.amount, b.amount)
     : sortAmountAsc(a.amount, b.amount);
+};
+
+export function filterDate(date, value, timeFilters) {
+  return moment().isSame(date, timeFilters[value]);
+};
+
+export function filterAmount(amount, selected) {
+  if (selected === 'income') return amount > 0;
+  if (selected === 'expense') return amount < 0;
+  return amount;
 };
--- client/src/utils/format.js
+++ client/src/utils/format.js
@@ -4,10 +4,32 @@ export function numberEuro(x) {
     .replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
 
+export function formatAmount (x) {
+  return x && Math.abs(x.toFixed(2));
+}
+
 export function numberValid(x) {
   return /^(\d+|\d{1,3}(\.\d{3})*)(,\d{1,2})?$/.test(x);
 }
 
+export function emailValid(x) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(x);
+}
+
 export function numberCalc(x) {
   return Number(x.replace(/\./g, '').replace(/,/, '.'));
+}
+
+export function formatDate(date) {
+  let d = new Date(date),
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
+
+  if (month.length < 2)
+      month = '0' + month;
+  if (day.length < 2)
+      day = '0' + day;
+
+  return [day, month, year].join('/');
 }
