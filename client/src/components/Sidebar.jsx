374e13e9d84b67216b5469ef47e3693fc21f3a47
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2021-03-14 15:54:39 +0900

    :+1: Modify sidebar components
    t

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -4,11 +4,16 @@ import { NavLink, withRouter } from 'react-router-dom'
 
 // Material-UI Core
 import { CssBaseline, AppBar, Drawer, Hidden, IconButton, Toolbar, Typography, MenuItem, MenuList, ListItemIcon, Fab, Zoom } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/core/styles'
 
 // Material-UI Icon
 import { Add } from '@material-ui/icons'
+import DashboardSharpIcon from '@material-ui/icons/DashboardSharp'
+import LibraryBooksSharpIcon from '@material-ui/icons/LibraryBooksSharp'
+import EqualizerSharpIcon from '@material-ui/icons/EqualizerSharp'
+import MeetingRoomSharpIcon from '@material-ui/icons/MeetingRoomSharp'
+import AccountBoxSharpIcon from '@material-ui/icons/AccountBoxSharp'
 
 // Context
 import { GlobalContext } from '../context/GlobalState'
@@ -17,24 +22,130 @@ import { GlobalContext } from '../context/GlobalState'
 import TransactionForm from './TransactionForm'
 
 // Utils
+import { checkDayTime } from '../utils/calculation.js';
 import { defaultMaterialTheme } from '../utils/colorTheme'
 
 const useStyles = makeStyles(() => ({
     addCircleIcon: {
       color: 'white'
     }
-  }));
+}));
 
-const Sidebar = ({ children }) => {
-  const classes = useStyles();
+const Sidebar = ({ children, location: { pathname } }) => {
   const { getToken, users, loadUser } = useContext(GlobalContext);
   const [open, setOpen] = useState(false);
   const token = getToken();
   const marginLeft = token ? '' : 'margin-left0'
-  const showAddButton = token !== null
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+    },
+    drawer: {
+      [theme.breakpoints.up('sm')]: {
+        width: drawerWidth,
+        flexShrink: 0,
+      },
+    },
+    appBar: {
+      boxShadow: 'none',
+      [theme.breakpoints.up('sm')]: {
+        width: token && `calc(100% - ${drawerWidth}px)`,
+        marginLeft: token && drawerWidth,
+        display: token ? 'none' : 'default',
+      },
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+      [theme.breakpoints.up('sm')]: {
+        display: 'none',
+      },
+      color: 'white',
+    },
+    drawerPaper: {
+      width: drawerWidth,
+      background: dayTime ? '#EAEBED' : '#232c2d',
+    },
+    content: {
+      flexGrow: 1,
+      marginTop: '55px',
+      [theme.breakpoints.up('sm')]: {
+        marginTop: 0,
+      },
+    },
+    textColor: {
+      color: dayTime ? '#232c2d' : 'white',
+      opacity: 0.8,
+    },
+    selectedColor: {
+      color: '#65bcbf',
+      fontWeight: 800,
+    },
+    appbarTitle: {
+      position: 'absolute',
+      left: '50%',
+      transform: 'translate(-50%)',
+      margin: '0 auto',
+      fontSize: '16px',
+      textTransform: 'uppercase',
+    },
+    fab: {
+      position: 'fixed',
+      bottom: theme.spacing(2),
+      right: theme.spacing(2),
+      boxShadow: 'none',
+    },
+    addCircleIcon: {
+      color: 'white'
+    },
+  }));
+
+  const drawerWidth = 240;
   const handleForm = useCallback(() => {
     setOpen(true);
   }, []);
+
+  const now = new Date(),
+        dayTime = checkDayTime(now);
+  const theme = useTheme();
+  const transition = {
+    enter: theme.transitions.duration.enteringScreen,
+    exit: theme.transitions.duration.leavingScreen,
+  };
+
+  const showAddButton =
+    token !== null && pathname !== '/profile' && pathname !== '/statistics';
+  const delay = { transitionDelay: `${showAddButton ? transition.exit : 0}ms` };
+  const classes = useStyles();
+  const iconSize = { fontSize: '25px' };
+  const drawerList = [
+    {
+      name: 'dashboard',
+      icon: <DashboardSharpIcon style={iconSize} />,
+    },
+    {
+      name: 'transactions',
+      icon: <LibraryBooksSharpIcon style={iconSize} />,
+    },
+    {
+      name: 'statistics',
+      icon: <EqualizerSharpIcon style={iconSize} />,
+    },
+    {
+      name: 'profile',
+      icon: <AccountBoxSharpIcon style={iconSize} />,
+    },
+    {
+      name: 'logout',
+      icon: <MeetingRoomSharpIcon style={iconSize} />,
+    },
+  ];
+  const getCurrentTitle = (pathname) => {
+    for (let i = 0; i < drawerList.length; i++) {
+      if ('/' + drawerList[i]['name'] === pathname)
+        return drawerList[i]['name'];
+    }
+  };
 
   useEffect(() => {
     token && loadUser();
@@ -55,21 +166,29 @@ const Sidebar = ({ children }) => {
            </div>
            <nav className="navigation">
              <MenuList>
-               <MenuItem to="/dashboard" component={NavLink}>
-                 <ListItemIcon>DashBoard</ListItemIcon>
-               </MenuItem>
-               <MenuItem to="/transactions" component={NavLink}>
-                 <ListItemIcon>Transactions</ListItemIcon>
-               </MenuItem>
-               <MenuItem to="/statistics" component={NavLink}>
-                 <ListItemIcon>Statistics</ListItemIcon>
-               </MenuItem>
-               <MenuItem to="/profile" component={NavLink}>
-                 <ListItemIcon>Profile</ListItemIcon>
-               </MenuItem>
-               <MenuItem to="/logout" component={NavLink}>
-                 <ListItemIcon>Logout</ListItemIcon>
-               </MenuItem>
+              {drawerList.map(({ name, icon }) => {
+              const path = '/' + name;
+              const itemColor = (path) => {
+              if (path === pathname) return classes.selectedColor;
+                return classes.textColor;
+              };
+              return (
+                <MenuItem
+                  key={name}
+                  to={path}
+                  component={NavLink}
+                  selected={path === pathname}
+                  style={{ minHeight: '48px' }}
+                >
+                  <ListItemIcon className={itemColor(path)} style={{ minWidth: '35px' }}>
+                    {icon}
+                  </ListItemIcon>
+                  <p className={itemColor(path)} style={{ fontSize: '14px' }}>
+                    {name}
+                  </p>
+                </MenuItem>
+                  );
+                })}
              </MenuList>
            </nav>
          </aside>
@@ -79,7 +198,7 @@ const Sidebar = ({ children }) => {
           {children}
         </main>
         <TransactionForm open={open} setOpen={setOpen} action='new' />
-        <Zoom in={showAddButton}>
+        <Zoom in={showAddButton} timeout={transition} style={delay} unmountOnExit>
           <div
             className="button-primary"
             onClick={handleForm}
